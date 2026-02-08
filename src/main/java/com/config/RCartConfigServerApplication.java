bd8bb1bd178c9bd601ef8b8d243da87645998606
abdulrasheedshaikgit <abdulrasheedshaik8@gmail.com>
2026-02-08 17:51:25 +0530

    Initial commit

---
+++ src/main/java/com/config/RCartConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RCartConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RCartConfigServerApplication.class, args);
+	}
+
+}
